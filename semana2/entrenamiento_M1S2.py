a5e2a4c054b573bb13a68f0665a46987ae6c5197
José Granados <jmgranadospy@gmail.com>
2025-05-04 16:22:57 -0500

    Actualizado entrenamiento_M1S2.py para una solución nueva.

--- semana2/entrenamiento_M1S2.py
+++ semana2/entrenamiento_M1S2.py
@@ -1,35 +1,64 @@
-cantidadCalificaciones = int(input("Ingrese la cantidad de calificaciones que va a ingresar: "))
-calificaciones = []
-contador = 0
+### Inicializar variables.
+ingresoCalificaciones = input("Ingrese la lista de calificaciones separadas por coma (Tenga en cuenta que calificaciones menores a 0 u mayores a 100 no serán tomadas.): ")
+nuevoStr = ""
+contadorMayor = 0
+contadorMenor = 0
 
-### Ingreso de las calificaciones.
-for i in range(cantidadCalificaciones):
-    while True:
-        calificacion = int(input(f"\nIngrese la {i+1}# calificación: "))
-        if 0 < calificacion and calificacion < 100:
-            calificaciones.append(calificacion)
-            break
-        else:
-            print(f"\nLa calificación debe estar entre 0 y 100. Intente nuevamente: ")
+### Ciclo for para recorrer el string ingresado por el usuario, agregando a un nuevo string únicamente los números, comas y guiones.
+for caracter in ingresoCalificaciones:
+    if caracter.isdigit() or caracter == "," or caracter == "-":
+        nuevoStr += caracter
+
+### Se crea una nueva lista, separando los valores por coma.
+calificaciones = []
+calificaciones = nuevoStr.split(",")
+
+### Ciclo for para convertir los elementos de la la lista en números enteros.
+for i in range(len(calificaciones)):
+    calificaciones[i] = int(calificaciones[i])
+
+### Ciclo for para eliminar aquellos números menores a 0 u mayores a 100.
+for i in calificaciones:
+    if i < 0 or i > 100:
+        calificaciones.remove(i)
 
 print("\n","="*50)
 
-### Verificación Aprobó - Reprobó.
-for i in range(cantidadCalificaciones):
-    if calificaciones[i] > 50:
+#### Se pide el valor para contar cuantas calificaciones lo superan.
+valorMayor = int(input("\n¿Cuantas calificaciones en la lista son mayores a este valor?: "))
+
+### Se le pide el valor para contar cuantas calificaciones son iguales.
+especificas = int(input("¿Cuantas calificaciones en la lista son iguales a este valor?: "))
+
+### ¿Cuanto es el mínimo necesario para aprobar?
+minimo = int(input("Ingrese la nota mínima para aprobar: "))
+
+### Ciclo para recorrer la lista final.
+for i in range(len(calificaciones)):
+
+    ### Verificación Aprobó - Reprobó para cada nota.
+    if calificaciones[i] >= minimo:
         print(f"\nNota {i}: {calificaciones[i]} Aprobó.")
     else:
         print(f"\nNota {i}: {calificaciones[i]} Reprobó. ")
 
-
-#### Contar calificaciones mayores
-valorMayor = int(input("\n¿Cuantas calificaciones en la lista son mayores a este valor?: "))
-
-for i in range(cantidadCalificaciones):
+    ### Verificación si la nota actual es mayor al valor ingresado previamente.
     if calificaciones[i] > valorMayor:
-        contador = contador + 1
+        contadorMayor = contadorMayor + 1
 
-print("En la lista de calificaciones hay",contador, "calificaciones mayores a ese valor.")
+    ### Verificación si la nota actual es igual al valor ingresado previamente.
+    if calificaciones[i] == especificas:
+        contadorMenor = contadorMenor + 1
 
-#### Promedio
-print("\nPromedio de las calificaciones: ",round(sum(calificaciones)/cantidadCalificaciones,2))
+print("\n","="*50)
+
+### Se imprimen resultados.
+print(f"\nEn la lista de calificaciones hay {contadorMayor} calificaciones mayores a {valorMayor}.\n")
+print(f"En la lista de calificaciones hay {contadorMenor} calificaciones iguales a {especificas}.\n")
+print("Promedio de las calificaciones: ",round(sum(calificaciones)/len(calificaciones),2)) 
+
+### Condicional para imprimir si se aprobó u reprobó el curso en base al promedio.
+if sum(calificaciones)/len(calificaciones) >= minimo:
+    print("Aprobó el curso.")
+else:
+    print("Reprobó el curso.")
