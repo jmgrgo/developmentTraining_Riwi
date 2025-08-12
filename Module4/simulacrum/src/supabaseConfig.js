4ece298517fff7c72567c7426f3956170fbd2dcd
José Granados <209109091+jmgrgo@users.noreply.github.com>
2025-08-12 01:49:53 -0500

    Update supabaseConfig.js

--- Module4/simulacrum/src/supabaseConfig.js
+++ Module4/simulacrum/src/supabaseConfig.js
@@ -1,8 +1,8 @@
 import './styles.css'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://aaaourjcdnawncyclcwa.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhYW91cmpjZG5hd25jeWNsY3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4ODIyMjMsImV4cCI6MjA3MDQ1ODIyM30.DFsctqTZdWKhFkueeNfU4evQoV24S9rjIrf-Ka4EcEw";
+const supabaseUrl = "";
+const supabaseKey = "";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
